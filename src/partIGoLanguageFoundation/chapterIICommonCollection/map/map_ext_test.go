c2332b2f525e0b01a489a123869f1457bc2a0d96
cjx <1067446576@qq.com>
2021-08-21 20:18:53 +0800

    map ext

---
+++ src/partIGoLanguageFoundation/chapterIICommonCollection/map/map_ext_test.go
@@ -0,0 +1,22 @@
+package my_map
+
+import "testing"
+
+func TestMapWithFunValue(t *testing.T) {
+	// Map 的 value 可以是一个方法
+	m := map[int]func(op int) int{}
+
+	m[1] = func(op int) int {
+		return op
+	}
+
+	m[2] = func(op int) int {
+		return op * op
+	}
+
+	m[3] = func(op int) int {
+		return op * op * op
+	}
+
+	t.Log(m[1](2), m[2](2), m[3](2))
+}
