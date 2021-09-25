3b215aa6da99f283601eb477493fcfb3e56507ef
cjx <1067446576@qq.com>
2021-09-25 17:53:59 +0800

    使用 Map 实现 Set 功能

--- src/partIGoLanguageFoundation/chapterIICommonCollection/set/set_test.go
+++ src/partIGoLanguageFoundation/chapterIICommonCollection/set/set_test.go
@@ -1,15 +1,36 @@
 package set
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 // 在 Go 语言中, Set 数据结构通过 Map 实现
 func TestMapForSet(t *testing.T) {
 	mySet := map[int]bool{}
 	mySet[1] = true
-	n := 1
-	if mySet[n] {
-		t.Logf("%d is exising", n)
+	setExist(mySet, 1)
+	t.Log(len(mySet))
+	mySet[3] = false
+	t.Log(len(mySet))
+	mySet[3] = true
+	t.Log(len(mySet))
+	delete(mySet, 1)
+	t.Log(len(mySet))
+}
+
+func setExist(mySet map[int]bool, key int) {
+	if mySet[key] {
+		fmt.Println("exising")
 	} else {
-		t.Logf("%d is not exising", n)
+		fmt.Println("not exising")
+	}
+}
+
+func setGet(mySet map[int]bool, key int) {
+	if mySet[key] {
+		fmt.Println(mySet[key])
+	} else {
+		fmt.Println("not exising")
 	}
 }
