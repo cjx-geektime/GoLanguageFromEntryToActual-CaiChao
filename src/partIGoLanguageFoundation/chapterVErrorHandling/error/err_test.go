2d03c12942cd953d3427590ecc9716eb89a64d79
cjx <1067446576@qq.com>
2021-09-27 03:58:40 +0800

    GOTO

--- src/partIGoLanguageFoundation/chapterVErrorHandling/error/err_test.go
+++ src/partIGoLanguageFoundation/chapterVErrorHandling/error/err_test.go
@@ -3,6 +3,7 @@ package err_test
 import (
 	"errors"
 	"fmt"
+	"strconv"
 	"testing"
 )
 
@@ -33,4 +34,40 @@ func TestGetFibonacci(t *testing.T) {
 	} else {
 		t.Log(v)
 	}
+}
+
+func GetFibonacci1(str string) {
+	var (
+		i    int
+		err  error
+		list []int
+	)
+	if i, err = strconv.Atoi(str); err == nil {
+		if list, err = GetFibonacci(i); err == nil {
+			fmt.Println(list)
+		} else {
+			fmt.Println("Error", err)
+		}
+	} else {
+		fmt.Println("Error", err)
+	}
+}
+
+func GetFibonacci2(str string) {
+	var (
+		i    int
+		err  error
+		list []int
+	)
+	if i, err = strconv.Atoi(str); err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+	if list, err = GetFibonacci(i); err != nil {
+
+		fmt.Println("Error", err)
+		return
+	}
+	fmt.Println(list)
+
 }
