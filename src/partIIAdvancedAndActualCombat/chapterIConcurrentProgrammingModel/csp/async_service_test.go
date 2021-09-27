417f270d4243cb79038f97d4bc5b03e5e3752cf8
cjx <1067446576@qq.com>
2021-09-27 20:42:00 +0800

    CSP并发机制

---
+++ src/partIIAdvancedAndActualCombat/chapterIConcurrentProgrammingModel/csp/async_service_test.go
@@ -0,0 +1,59 @@
+package concurrency
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func service() string {
+	time.Sleep(time.Millisecond * 50)
+	return "service Done"
+}
+
+func otherTask() {
+	fmt.Println("working on something else")
+	time.Sleep(time.Millisecond * 100)
+	fmt.Println("Task is done.")
+}
+
+func TestService(t *testing.T) {
+	fmt.Println(service())
+	otherTask()
+}
+
+func AsyncServiceByChan() chan string {
+	retCh := make(chan string)
+	go func() {
+		ret := service()
+		fmt.Println("returned result")
+		retCh <- ret
+		fmt.Println("service exited")
+	}()
+	return retCh
+}
+
+func AsyncServiceByBufferChan() chan string {
+	retCh := make(chan string, 1)
+	go func() {
+		ret := service()
+		fmt.Println("returned result")
+		retCh <- ret
+		fmt.Println("service exited")
+	}()
+	return retCh
+}
+
+func TestAsyncService(t *testing.T) {
+	retCh := AsyncServiceByChan()
+	otherTask()
+	fmt.Println(<-retCh)
+	time.Sleep(time.Second * 1)
+}
+
+func TestAsyncService2(t *testing.T) {
+	retCh := AsyncServiceByBufferChan()
+	otherTask()
+	fmt.Println(<-retCh)
+	time.Sleep(time.Second * 1)
+}
