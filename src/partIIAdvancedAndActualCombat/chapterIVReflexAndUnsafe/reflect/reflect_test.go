73ef7591683c011179b983b0d889ea06575c36b1
cjx <1067446576@qq.com>
2021-10-07 21:59:52 +0800

    反射

---
+++ src/partIIAdvancedAndActualCombat/chapterIVReflexAndUnsafe/reflect/reflect_test.go
@@ -0,0 +1,58 @@
+package reflect_test
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestTypeAndValue(t *testing.T) {
+	var f int64 = 10
+	t.Log(reflect.TypeOf(f), reflect.ValueOf(f))
+	t.Log(reflect.ValueOf(f).Type())
+}
+
+//检查反射类型
+//用空接口接收任意类型
+func CheckType(v interface{}) {
+	t := reflect.TypeOf(v)
+	switch t.Kind() {
+	case reflect.Float32, reflect.Float64:
+		fmt.Println("Float")
+	case reflect.Int, reflect.Int32, reflect.Int64:
+		fmt.Println("Integer")
+	default:
+		fmt.Println("Unknow", t)
+	}
+}
+
+func TestCheckType(t *testing.T) {
+	var f float64 = 32
+	CheckType(f)
+	CheckType(&f)
+}
+
+//通过反射调用结构体
+type Employee struct {
+	EmployeeID string
+	Name       string `format:"normal"`
+	Age        int
+}
+
+func (e *Employee) UpdateAge(newVal int) {
+	e.Age = newVal
+}
+
+func TestInvokeByName(t *testing.T) {
+	e := &Employee{"1", "Mike", 30}
+	//按名字获取成员
+	t.Logf("Name: value(%[1]v), Type(%[1]T) ", reflect.ValueOf(*e).FieldByName("Name"))
+	if nameField, ok := reflect.TypeOf(*e).FieldByName("Name"); !ok {
+		t.Error("Failed to get 'name' field.")
+	} else {
+		t.Log("Tag:format", nameField.Tag.Get("format"))
+	}
+	reflect.ValueOf(e).MethodByName("UpdateAge").
+		Call([]reflect.Value{reflect.ValueOf(1)})
+	t.Log("Updated Age: ", e)
+}
